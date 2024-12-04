63d2af889766e41318047c771ec0f9331ea92a1d
MfgIsiaka <mfg.isiaka@gmail.com>
2024-12-04 21:55:43 +0300

    Added allowed hosts

--- Website/settings.py
+++ Website/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-&-&5e+mz2a726wun&@m8=rty6)n7+d2$%_m+i@c=#8gx&6%rih
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
